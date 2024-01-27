6de327f05f1dfd9cf41780bd781d7a7ccfa84c33
fatmaCH <91056116+CHellamaFatma@users.noreply.github.com>
2024-01-27 22:39:06 +0100

    Add files via upload

---
+++ service.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <title>SERVICE</title>
+    <meta name="viewport" content="width=dvice-width, intial-scale=1.0">
+    <link rel="stylesheet" href="cssFILES/sttyle.css">
+    <link rel=" stylesheet " href="cssFILES/fati.css">
+    <link rel="stylesheet " href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/3.6.0/remixicon.css ">
+    <script src="https://kit.fontawesome.com/a076d05399.js "></script>
+
+<style>
+    .button-63 {
+  align-items: center;
+  background-image: linear-gradient(144deg,#AF40FF, #5B42F3 50%,#00DDEB);
+  border: 0;
+  border-radius: 8px;
+  box-shadow: rgba(151, 65, 252, 0.2) 0 15px 30px -5px;
+  box-sizing: border-box;
+  color: #FFFFFF;
+  display: flex;
+  font-family: Phantomsans, sans-serif;
+  font-size: 20px;
+  justify-content: center;
+  line-height: 1em;
+  max-width: 100%;
+  min-width: 140px;
+  padding: 19px 24px;
+  text-decoration: none;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  white-space: nowrap;
+  cursor: pointer;
+}
+
+.button-63:active,
+.button-63:hover {
+  outline: 0;
+}
+
+@media (min-width: 768px) {
+  .button-63 {
+    font-size: 24px;
+    min-width: 196px;
+  }
+}
+    </style>
+</head>
+
+<body>
+    <header class="header ">
+        <a  class="logo " href ="service.php">FANC</a>
+
+        
+    </header>
+    <br>
+    <section class="home " id="home ">
+        <div class="home-content ">
+
+      <h2> WELCOME ADMISTRATION </h2>
+  
+      
+ <button class="button-63" role="button"> <a href = "index.php"> Notifications </a> </button>
+  <br>
+<button class="button-63" role="button" ><a href = "expert.php">  ADD </a> </button>
+<br>
+<button class="button-63" role="button"> <a href = "listrappot.php">Rapports </a> </button>
+
+
+
+</div>
+
+
+<div class="profession-container ">
+            <div class="profession-box ">
+                <div class="profession " style="--i:0; ">
+                    <i class='bx bx-code-alt'></i>
+                    <h3>Assurant</h3>
+                </div>
+                <div class="profession " style="--i:1; ">
+                    <i class='bx bx-camera'></i>
+                    <h3>Expert</h3>
+                </div>
+                <div class="profession " style="--i:2; ">
+                    <i class='bx bx-palette'></i>
+                    <h3>Mecanicien</h3>
+                </div>
+                <div class="circle "></div>
+            </div>
+
+            <div class="overlay "></div>
+        </div>
+    </section>
+   
+
+
+</body>
+
+</html>
